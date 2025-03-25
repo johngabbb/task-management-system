dd492213c6d6aa827b6693b28ceb47d20de66783
John Gabriel <johngaelreyes@gmail.com>
2025-03-25 21:19:12 +0800

    Project init

---
+++ task-management-system-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-neutral-800 flex justify-center items-center h-screen w-screen"></div>
+    </>
+  );
+}
+
+export default App;
