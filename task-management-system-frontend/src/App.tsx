581a00891bc6340d66f0f41268adaf8fd49b527b
John Gabriel <johngaelreyes@gmail.com>
2025-03-25 22:17:33 +0800

    ShadCn Components + login init

--- task-management-system-frontend/src/App.tsx
+++ task-management-system-frontend/src/App.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import LoginPage from "./components/Pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <>
-      <div className="bg-neutral-800 flex justify-center items-center h-screen w-screen"></div>
+      <div className="bg-neutral-800 h-screen">
+        <LoginPage />
+      </div>
     </>
   );
 }
--- task-management-system-frontend/src/components/Pages/LoginPage/LoginPage.tsx
+++ task-management-system-frontend/src/components/Pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,20 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
 
 interface Props {}
 
 const LoginPage = (props: Props) => {
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <div className="h-full flex items-center justify-center">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+          </CardHeader>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
