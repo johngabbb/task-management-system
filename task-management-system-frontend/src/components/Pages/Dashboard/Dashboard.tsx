73010edaacad5ced8fde3dcef1f82ccc77d2ad9e
jreyes\Nimbyx <jreyes@nymbix.com>
2025-04-01 16:52:53 +0800

    Grid Dashboard

--- task-management-system-frontend/src/components/Pages/Dashboard/Dashboard.tsx
+++ task-management-system-frontend/src/components/Pages/Dashboard/Dashboard.tsx
@@ -15,21 +15,38 @@ const Dashboard = (props: Props) => {
 
   return (
     <>
-      <div className="h-full w-full grid grid-cols-[300px_1fr] grid-rows-[80px_1fr] bg-neutral-900">
-        <div className="border-1 text-white flex items-center justify-center">LOGO + NAME</div>
-        <div className="border-1 text-white flex items-center justify-center">NAVBAR</div>
-        <div className="border-1 text-white flex items-center justify-center">SIDEBAR</div>
-        <div className="border-1 text-white flex items-center justify-center">MAIN CONTENT</div>
-        {/* <SideBar />
+      <div className="h-screen w-full grid grid-cols-[300px_1fr] grid-rows-[56px_112px_1fr] bg-black text-white">
+      <div className="flex items-center justify-center border-r border-b border-gray-700">
+        LOGO + NAME
+      </div>
+      <div className="flex items-center justify-center border-b border-gray-700">
+        NAVBAR
+      </div>
+      
+      <div className="flex items-center justify-center border-r border-b border-gray-700">
+        PROJECT NAME + IMAGE
+      </div>
+      <div className="flex items-center justify-center border-b border-gray-700">
+        Sub header
+      </div>
+      
+      <div className="flex items-center justify-center border-r border-gray-700">
+        <SideBar/>
+      </div>
+      <div className="flex items-center justify-center">
+        Main Content
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Dashboard;
+
+{/* <SideBar />
         <div className="bg-inherit">
           <NavBar />
           <div>
             <Card className="w-10">Card test</Card>
           </div>
         </div> */}
-      </div>
-    </>
-  );
-};
-
-export default Dashboard;
