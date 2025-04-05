7251606852a0ee3649bc2ba366b2120aca30f72c
John Gabriel <johngaelreyes@gmail.com>
2025-04-05 23:14:02 +0800

    Add card popup dialog

--- task-management-system-frontend/src/components/Pages/TasksPage/TaskCard/TaskCard.tsx
+++ task-management-system-frontend/src/components/Pages/TasksPage/TaskCard/TaskCard.tsx
@@ -1,16 +1,15 @@
-import { AlertDialog, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardTitle } from "@/components/ui/card";
-import React from "react";
+import { Dialog, DialogContent, DialogTrigger, DialogClose } from "@/components/ui/dialog";
 
 type Props = {};
 
 const TaskCard = (props: Props) => {
   return (
     <>
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
+      <Dialog>
+        <DialogTrigger asChild>
           <Button className="h-50 p-6 border-1 border-neutral-700 hover:bg-black cursor-pointer text-white w-full flex items-start flex-col justify-between">
+            {/* Button content remains the same */}
             <div className="text-left w-full overflow-hidden">
               <div className="text-sm font-bold w-full break-all overflow-hidden text-ellipsis whitespace-normal">
                 3Shape 3.0.1 - Case downloader continuous to log cases without file attachments as
@@ -30,8 +29,12 @@ const TaskCard = (props: Props) => {
               <div className="flex-1 text-sm text-neutral-500 mt-2 text-right">HT-1824</div>
             </div>
           </Button>
-        </AlertDialogTrigger>
-      </AlertDialog>
+        </DialogTrigger>
+        <DialogContent className="!max-w-none !w-[1020px] !h-[720px] bg-neutral-950 border-1 border-neutral-700 flex flex-col justify-center items-center">
+          {/* Simple X button */}
+          <DialogClose className=" text-neutral-500 hover:text-white"></DialogClose>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
